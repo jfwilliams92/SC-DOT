bb12434362695ca077c507d9aec32a08f11c7505
JW <jimmy.williams92@outlook.com>
2020-04-27 16:45:34 -0400

    change app layout add violin

--- app.py
+++ app.py
@@ -36,68 +36,64 @@ route_name_dict.insert(0, {'label': 'All', 'value': 'All'})
 app.layout = html.Div([
     html.Div([
         html.Div([
-            html.H6('Route Type Slicer'),
-            dcc.Dropdown(
-                id='route-types',
-                options=route_dict,
-                value='All',
-                multi=True
-            )
-        ], className="three columns"),
+            html.Div([
+                html.H6('Route Type Slicer'),
+                dcc.Dropdown(
+                    id='route-types',
+                    options=route_dict,
+                    value=['All'],
+                    multi=True
+                )
+            ], className="row"),
+            html.Div([
+                html.H6('County Slicer'),
+                dcc.Dropdown(
+                    id='county-names',
+                    options=county_dict,
+                    value=['All'],
+                    multi=True,
+                )
+            ], className="row"),
+            html.Div([
+                html.H6('Route Slicer'),
+                dcc.Dropdown(
+                    id='route-names',
+                    options=route_name_dict,
+                    value=['All'],
+                    multi=True,
+                )
+            ], className="row"),
+            html.Div([
+                html.H6('Values'),
+                dcc.RadioItems(
+                id='scale',
+                options=[
+                    {'label': 'AADT', 'value': 'AADT'},
+                    {'label': 'Log10AADT', 'value': 'Log10AADT'},
+                    {'label': 'Total Percent Change', 'value': 'Percent Change'}
+                ],
+                value='Log10AADT'
+                )  
+            ], className="row"),
+            html.Div([
+                html.H6('Map Background'),
+                dcc.RadioItems(
+                id='background',
+                options=[
+                    {'label': 'Light', 'value': 'light'},
+                    {'label': 'Dark', 'value': 'dark'}
+                ],
+                value='dark'
+                )  
+            ], className="row")    
+        ], className="three columns", style={"padding": 25}),
         html.Div([
-            html.H6('County Slicer'),
-            dcc.Dropdown(
-                id='county-names',
-                options=county_dict,
-                value='All',
-                multi=True,
-            )
-        ], className="three columns"),
-        html.Div([
-            html.H6('Route Slicer'),
-            dcc.Dropdown(
-                id='route-names',
-                options=route_name_dict,
-                value='All',
-                multi=True,
-            )
-        ], className="three columns"),
-        html.Div([
-            html.H6('Values'),
-            dcc.RadioItems(
-            id='scale',
-            options=[
-                {'label': 'AADT', 'value': 'AADT'},
-                {'label': 'Log10AADT', 'value': 'Log10AADT'},
-                {'label': 'Total Percent Change', 'value': 'Percent Change'}
-            ],
-            value='Log10AADT'
-            )  
-        ], className="two columns"),
-        html.Div([
-            html.H6('Map Background'),
-            dcc.RadioItems(
-            id='background',
-            options=[
-                {'label': 'Light', 'value': 'light'},
-                {'label': 'Dark', 'value': 'dark'}
-            ],
-            value='dark'
-            )  
-        ], className="one column")
-        
+            dcc.Graph(id='scatter-geo')
+        ], className="nine columns")
     ], className="row"),
-    html.Div([
-        dcc.Slider(
-            id='crossfilter-year--slider',
-            min=traffic_df['year'].min(),
-            max=traffic_df['year'].max(),
-            value=traffic_df['year'].max(),
-            marks={int(year): str(int(year)) for year in traffic_df['year'].unique()},
-            step=1.0,
-        ),
-    ], style={'padding': 50}),
-    html.Div([dcc.Graph( id='scatter-geo')])
+    html.Div(
+        [dcc.Graph(id='year-plot')], className="row"
+    )
 ])
 
 @app.callback(
@@ -110,19 +106,21 @@ app.layout = html.Div([
 )
 def update_map(route_type, county_name, route_name, scale, map_background):
   
-    if (not route_type) or (route_type=='All'):
+    if len(route_type) < 1 or route_type==['All']:
         route_type = list(traffic_df.route_type.unique())
 
-    if (not county_name) or (county_name == 'All'):
+    if len(county_name) < 1 or county_name==['All']:
         county_name = list(traffic_df.county_name.unique())
 
-    if (not route_name) or (route_name == 'All'):
+    if len(route_name) < 1 or route_name==['All']:
         route_name = list(traffic_df.route.unique())
 
     route_type_mask = traffic_df.route_type.isin(route_type)
     county_mask = traffic_df.county_name.isin(county_name)
     route_mask = traffic_df.route.isin(route_name)
-    filter_mask = route_type_mask & county_mask & route_mask
+    year_mask = traffic_df.year == 2018
+    filter_mask = route_type_mask & county_mask & route_mask & year_mask
+
 
     plot_df = traffic_df[filter_mask]
 
@@ -159,26 +157,66 @@ def update_map(route_type, county_name, route_name, scale, map_background):
         ),
         text = plot_df['route_leg_descrip'] + " - AverageDailyTraffic: " + plot_df.average_daily_traffic.astype(str)
     )]
-    
-                      
+               
     layout = go.Layout(
-        title = 'Average Daily Traffic in SC',
+        title = title,
         #autosize = True,
         hovermode = 'closest',
         showlegend = False,
         mapbox = dict(
             accesstoken = mapboxkey,
             bearing = 0,
-            center = dict(lat = 34, lon = -81),
+            center = dict(lat = 33.8, lon = -81),
             pitch = 0,
-            zoom = 6.5,
+            zoom = 6.6,
             style = map_background
             ),
-        height=950,
+        height=800,
         uirevision=True
         )
 
     return {'data': data, 'layout': layout} 
+
+@app.callback(
+    dash.dependencies.Output('year-plot', 'figure'),
+    [dash.dependencies.Input('route-types', 'value'),
+    dash.dependencies.Input('county-names', 'value'),
+    dash.dependencies.Input('route-names', 'value'),
+    dash.dependencies.Input('scale', 'value')]
+)
+def update_yearplot(route_type, county_name, route_name, scale):
+  
+    if len(route_type) < 1 or route_type==['All']:
+        route_type = list(traffic_df.route_type.unique())
+
+    if len(county_name) < 1 or county_name==['All']:
+        county_name = list(traffic_df.county_name.unique())
+
+    if len(route_name) < 1 or route_name==['All']:
+        route_name = list(traffic_df.route.unique())
+
+    route_type_mask = traffic_df.route_type.isin(route_type)
+    county_mask = traffic_df.county_name.isin(county_name)
+    route_mask = traffic_df.route.isin(route_name)
+    filter_mask = route_type_mask & county_mask & route_mask
+
+    plot_df = traffic_df[filter_mask]
+
+    # map configurations
+    if scale == 'AADT':
+        vals = plot_df['average_daily_traffic']
+        title = 'Average Daily Traffic'
+    elif scale == 'Log10AADT':
+        vals = np.log10(plot_df['average_daily_traffic'])
+        title = 'Log10(Average Daily Traffic)'
+    elif scale == 'Percent Change':
+        vals = plot_df['total_pct_change']
+        title = 'Total Pct Change'
+
+    data = [go.Violin(y=vals, x=plot_df["year"], box_visible=True, meanline_visible=True, opacity=0.6)]
+    layout = go.Layout(title=title)
+
+    return {'data': data, 'layout': layout}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
